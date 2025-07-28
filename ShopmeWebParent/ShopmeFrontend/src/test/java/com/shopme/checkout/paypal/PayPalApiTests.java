662f131201a5fcca2c24c065e184523557517698
FatCat0712 <maisonhai456@gmail.com>
2025-07-28 17:43:12 +0700

    fix leak key

--- ShopmeWebParent/ShopmeFrontend/src/test/java/com/shopme/checkout/paypal/PayPalApiTests.java
+++ ShopmeWebParent/ShopmeFrontend/src/test/java/com/shopme/checkout/paypal/PayPalApiTests.java
@@ -1,17 +1,26 @@
 package com.shopme.checkout.paypal;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.*;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class PayPalApiTests {
     private static final String BASE_URL = "https://api.sandbox.paypal.com";
     private static final String GET_ORDER_API = "/v2/checkout/orders/";
-    private static final String CLIENT_ID = "AZb9q-xIQxAKTQS1XOuhecvxCXIHaqa08KlJWmvf1rnTdKjiTeSnyqEAa7mx00q1vr_h5Nx7p1hJqX7B";
-    private static final String CLIENT_SECRET = "EETu6ZHLXtONwP_HqbSyVsAjqORKQscokK0S0fczPP-yEgvyigHN0sWcXpFZ64bK0Z4hyS-Ls8OlWQnl";
+
+     @Value("${paypal.client-id}")
+    private String CLIENT_ID;
+
+     @Value("${paypal.client-secret}")
+    private String CLIENT_SECRET;
 
     @Test
     public void testGetOrderDetails() {
@@ -28,7 +37,7 @@ public class PayPalApiTests {
 
         ResponseEntity<PaypalOrderResponse> response = restTemplate.exchange(requestURL, HttpMethod.GET, request, PaypalOrderResponse.class);
 
-        PaypalOrderResponse orderResponse = (PaypalOrderResponse)response.getBody();
+        PaypalOrderResponse orderResponse = response.getBody();
 
         System.out.println("Order ID: " + orderResponse.getId());
         System.out.println("Validated: " + orderResponse.validate(orderId));
