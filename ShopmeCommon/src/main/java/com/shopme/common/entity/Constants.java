fbe3e1eeeff421c0a9a8ed65b3eaec51451ada44
FatCat0712 <maisonhai456@gmail.com>
2025-08-04 15:09:14 +0700

    code supabase S3 Utility class

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Constants.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Constants.java
@@ -4,11 +4,15 @@ public class Constants {
     public static final String SUPABASE_URI;
 
     static  {
-        String projectId = "vsovwlewklunhjemktxg";
-        String bucketName = "shopme-files";
+        String projectId = System.getenv("SUPABASE_PROJECT_ID");
+        String bucketName = System.getenv("SUPABASE_BUCKET_NAME");
         String pattern = "https://%s.storage.supabase.co/storage/v1/object/public/%s";
 
         SUPABASE_URI = String.format(pattern, projectId, bucketName);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(SUPABASE_URI);
     }
 
 }
---
+++ ShopmeWebParent/ShopmeBackend/src/test/java/com/shopme/admin/SupabaseS3UtilTests.java
@@ -0,0 +1,45 @@
+package com.shopme.admin;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.List;
+
+public class SupabaseS3UtilTests {
+    @Test
+    public void testListFolder() {
+        String folderName = "user-photos";
+        List<String> listKeys = SupabaseS3Util.listFolder(folderName);
+        listKeys.forEach(System.out::println);
+    }
+
+    @Test
+    public void testUploadFile() {
+        String folderName = "test-upload";
+        String fileName = "file.zip";
+        String filePath = "D:\\" + fileName;
+        try {
+            InputStream inputStream = new FileInputStream(filePath);
+            SupabaseS3Util.uploadFile(folderName, fileName, inputStream);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Test
+    public void testDeleteFile() {
+        String fileName = "test-upload/file.zip";
+        SupabaseS3Util.deleteFile(fileName);
+    }
+
+    @Test
+    public void testRemoveFolder() {
+        String folderName = "test-upload";
+        SupabaseS3Util.removeFolder(folderName);
+    }
+
+
+}
